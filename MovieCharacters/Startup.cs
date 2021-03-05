3ba54299b730c217926522b32af2f196d1ac6a9f
ali123321123 <ali2016zedan@gmail.com>
2021-03-05 17:58:19 +0100

    Added AutoMapper
    Closes #12

--- MovieCharacters/Startup.cs
+++ MovieCharacters/Startup.cs
@@ -28,6 +28,7 @@ namespace MovieCharacters
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Added Automapper
             services.AddAutoMapper(typeof(Startup));
             services.AddControllers();
             services.AddSwaggerGen(c =>
