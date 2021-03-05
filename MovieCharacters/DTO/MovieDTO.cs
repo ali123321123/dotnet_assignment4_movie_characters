daeace310d387b8e83a333910b7d66e8f98f23c9
“Thanh” <thatra94@gmail.com>
2021-03-05 13:06:59 +0100

    controller, models, dto

---
+++ MovieCharacters/DTO/MovieDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieCharacters.DTO
+{
+    public class MovieDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Director { get; set; }
+        public string Picture { get; set; }
+        public string Trailer { get; set; }
+    }
+}
--- MovieCharacters/Models/DomainModels/Movie.cs
+++ MovieCharacters/Models/DomainModels/Movie.cs
@@ -10,13 +10,21 @@ namespace MovieCharacters.Models.DomainModels
     {
         [Key]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Title { get; set; }
+        [MaxLength(15)]
         public string Genre { get; set; }
+        [MaxLength(4)]
         public int ReleaseYear { get; set; }
+        [MaxLength(50)]
         public string Director { get; set; }
+        [MaxLength(50)]
         public string Picture { get; set; }
+        [MaxLength(50)]
         public string Trailer { get; set; }
-        public int FranchiseId { get; set; }
+        public int? FranchiseId { get; set; }
         public Franchise Franchise { get; set; }
+        public ICollection<Character> Characters { get; set; }
     }
 }
